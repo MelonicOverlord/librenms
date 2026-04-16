c96dca7ed5ca06aed414318843557b99e361c587
gentoo.linux <73000042+MelonicOverlord@users.noreply.github.com>
2026-04-16 17:54:03 +0100

    Update VOSS IS-IS Discovery

--- LibreNMS/OS/Voss.php
+++ LibreNMS/OS/Voss.php
@@ -1,21 +1,42 @@
-* Voss.php
+<?php
+
+/**
+ * Voss.php
  *
- * Extreme VOSS ISIS Neighbors
+ * Extreme VOSS ISIS Adjacencies
+ * Adapted from Iosxe.php
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- * Code repurposed from Cisco IOS-XE ISIS Neighbors
- * LibreNMS/OS/Iosxe.php
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * @link       https://www.librenms.org
+ */
 
 namespace LibreNMS\OS;
 
+use App\Facades\PortCache;
 use App\Models\IsisAdjacency;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Log;
+use LibreNMS\DB\SyncsModels;
 use LibreNMS\Interfaces\Discovery\IsIsDiscovery;
 use LibreNMS\Interfaces\Polling\IsIsPolling;
+use LibreNMS\Interfaces\Polling\OSPolling;
 use LibreNMS\Util\IP;
 use SnmpQuery;
 
+class Voss extends Shared\Extreme implements IsIsDiscovery, IsIsPolling
     /**
      * Array of shortened ISIS codes
      *
@@ -30,39 +51,37 @@ use SnmpQuery;
     public function discoverIsIs(): Collection
     {
         // Check if the device has any ISIS enabled interfaces
-        $circuits = SnmpQuery::enumStrings()->walk('ISIS-MIB::isisCircIfIndex');
+        $circuits = SnmpQuery::enumStrings()->walk('ISIS-MIB-LEGACY::isisCirc');
         $adjacencies = new Collection;
 
         if ($circuits->isValid()) {
             $circuits = $circuits->table(1);
-            $adjacencies_data = SnmpQuery::enumStrings()->walk('ISIS-MIB::isisISAdjState')->table(2);
+            $adjacencies_data = SnmpQuery::enumStrings()->walk('ISIS-MIB-LEGACY::isisISAdj')->table(2);
 
             foreach ($adjacencies_data as $circuit_index => $adjacency_list) {
                 foreach ($adjacency_list as $adjacency_index => $adjacency_data) {
-                    if (empty($circuits[$circuit_index]['ISIS-MIB::isisCircIfIndex'])) {
+                    if (empty($circuits[$circuit_index]['ISIS-MIB-LEGACY::isisCircPassiveCircuit'])) {
                         continue;
                     }
 
-                    if (($circuits[$circuit_index]['ISIS-MIB::isisCircPassiveCircuit'] ?? 'true') == 'true') {
+                    if (($circuits[$circuit_index]['ISIS-MIB-LEGACY::isisCircPassiveCircuit'] ?? 'true') == 'true') {
                         continue; // Do not poll passive interfaces and bad data
                     }
 
                     $adjacencies->push(new IsisAdjacency([
                         'device_id' => $this->getDeviceId(),
                         'index' => "[$circuit_index][$adjacency_index]",
-                        'ifIndex' => $circuits[$circuit_index]['ISIS-MIB::isisCircIfIndex'],
-                        'port_id' => $this->ifIndexToId($circuits[$circuit_index]['ISIS-MIB::isisCircIfIndex']),
-                        'isisCircAdminState' => $circuits[$circuit_index]['ISIS-MIB::isisCircAdminState'] ?? 'down',
-                        'isisISAdjState' => $adjacency_data['ISIS-MIB::isisAdjState'] ?? 'down',
-                        'isisISAdjNeighSysType' => Arr::get($this->isis_codes, $adjacency_data['ISIS-MIB::isisISAdjNeighSysType'] ?? '', 'unknown'),
-                        'isisISAdjNeighSysID' => $this->formatIsIsId($adjacency_data['ISIS-MIB::isisISAdjNeighSysID'] ?? ''),
-                        'isisISAdjNeighPriority' => $adjacency_data['ISIS-MIB::isisISAdjNeighPriority'] ?? '',
-                        'isisISAdjLastUpTime' => $this->parseAdjacencyTime($adjacency_data['ISIS-MIB::isisISAdjLastUpTime'] ?? 0),
-                        'isisISAdjAreaAddress' => implode(',', array_map([$this, 'formatIsIsId'], $adjacency_data['ISIS-MIB::isisISAdjAreaAddress'] ?? [])),
-                        'isisISAdjIPAddrType' => implode(',', $adjacency_data['ISIS-MIB::isisISAdjIPAddrType'] ?? []),
-                        'isisISAdjIPAddrAddress' => implode(',', array_map(function ($ip) {
-                            return (string) IP::fromHexstring($ip, true);
-                        }, $adjacency_data['ISIS-MIB::isisISAdjIPAddrAddress'] ?? [])),
+                        'ifIndex' => $circuits[$circuit_index]['ISIS-MIB-LEGACY::isisCircIfIndex'],
+                        'port_id' => PortCache::getIdFromIfIndex($circuits[$circuit_index]['ISIS-MIB-LEGACY::isisCircIfIndex'], $this->getDevice()),
+                        'isisCircAdminState' => $circuits[$circuit_index]['ISIS-MIB-LEGACY::isisCircAdminState'] ?? 'down',
+                        'isisISAdjState' => $adjacency_data['ISIS-MIB-LEGACY::isisAdjState'] ?? 'down',
+                        'isisISAdjNeighSysType' => Arr::get($this->isis_codes, $adjacency_data['ISIS-MIB-LEGACY::isisISAdjNeighSysType'] ?? '', 'unknown'),
+                        'isisISAdjNeighSysID' => $this->formatIsIsId($adjacency_data['ISIS-MIB-LEGACY::isisISAdjNeighSysID'] ?? ''),
+                        'isisISAdjNeighPriority' => $adjacency_data['ISIS-MIB-LEGACY::isisISAdjNeighPriority'] ?? '',
+                        'isisISAdjLastUpTime' => $this->parseAdjacencyTime($adjacency_data['ISIS-MIB-LEGACY::isisISAdjLastUpTime'] ?? 0),
+                        'isisISAdjAreaAddress' => implode(',', array_map($this->formatIsIsId(...), $adjacency_data['ISIS-MIB-LEGACY::isisISAdjAreaAddress'] ?? [])),
+                        'isisISAdjIPAddrType' => implode(',', $adjacency_data['ISIS-MIB-LEGACY::isisISAdjIPAddrType'] ?? []),
+                        'isisISAdjIPAddrAddress' => implode(',', array_map(fn ($ip) => (string) IP::fromHexString($ip, true), $adjacency_data['ISIS-MIB-LEGACY::isisISAdjIPAddrAddress'] ?? [])),
                     ]));
                 }
             }
@@ -73,19 +92,36 @@ use SnmpQuery;
 
     public function pollIsIs($adjacencies): Collection
     {
-        $states = SnmpQuery::enumStrings()->walk('ISIS-MIB::isisAdjState')->values();
+        $states = SnmpQuery::enumStrings()->walk('ISIS-MIB-LEGACY::isisAdjState')->values();
         $up_count = array_count_values($states)['up'] ?? 0;
 
         if ($up_count !== $adjacencies->count()) {
-            echo 'New Adjacencies, running discovery';
+            Log::info('New Adjacencies, running discovery');
 
             return $this->fillNew($adjacencies, $this->discoverIsIs());
         }
 
-        $uptime = SnmpQuery::walk('ISIS-MIB::isisISAdjLastUpTime')->values();
+        $uptime = SnmpQuery::walk('ISIS-MIB-LEGACY::isisCircLastUpTime')->values();
 
-        return $adjacencies->each(function (IsisAdjacency $adjacency) use ($states, $uptime) {
-            $adjacency->isisISAdjState = $states['ISIS-MIB::isisAdjState' . $adjacency->index] ?? $adjacency->isisISAdjState;
-            $adjacency->isisISAdjLastUpTime = $this->parseAdjacencyTime($uptime['ISIS-MIB::isisCircLastUpTime' . $adjacency->index] ?? 0);
+        return $adjacencies->each(function ($adjacency) use ($states, $uptime): void {
+            $adjacency->isisISAdjState = $states['ISIS-MIB-LEGACY::isisAdjState' . $adjacency->index] ?? $adjacency->isisISAdjState;
+            $adjacency->isisISAdjLastUpTime = $this->parseAdjacencyTime($uptime['ISIS-MIB-LEGACY::isisCircLastUpTime' . $adjacency->index] ?? 0);
         });
     }
+
+    /**
+     * Converts SNMP time to int in seconds
+     *
+     * @param  string|int  $uptime
+     * @return int
+     */
+    protected function parseAdjacencyTime($uptime): int
+    {
+        return (int) round(max($uptime, 1) / 100);
+    }
+
+    protected function formatIsIsId(string $raw): string
+    {
+        return str_replace(' ', '.', trim($raw));
+    }
+}
